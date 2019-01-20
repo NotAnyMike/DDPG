97570759831e2c3367ff4d8a1dacc288ffbcfcc8
NotAnyMike <mikewoodcockc@gmail.com>
2019-01-20 09:36:11 +0000

    printing info

--- ddpg.py
+++ ddpg.py
@@ -1,15 +1,15 @@
 # Imports
 import gym
 import tensorflow as tf
-from utils import create_nn, get_action, get_vars
+from utils import create_nn, get_action, get_vars, count_vars
 from replay_buffer import ReplayBuffer
 from pdb import set_trace
 
 # Set Hyper-parameters
-num_epochs   = 80
-ep_per_epoch = 200 #1000
+num_epochs   = 1000
+ep_per_epoch = 5000 #1000
+random_steps = 10000
 star_steps   = 100
-delay        = 10    # The number of steps deleyed to update target networks
 hidden_sizes = [400,300]
 buffer_size  = int(1e6)
 batch_size   = 100
@@ -20,14 +20,16 @@ p            = 0.995
 
 # Create environment
 env = gym.make('BipedalWalker-v2')
-env.render()
+#env.render()
 
 # Get imporant variables
 action_space = env.action_space
 obs_space    = env.observation_space
 action_dim   = action_space.shape[0]
 obs_dim      = obs_space.shape[0]
-max_act_val  = action_space.high[0] # TODO get high values for each dim
+max_act      = action_space.high[0] # TODO get high values for each dim
+print("obs", obs_dim)
+print("act", action_dim)
 
 # Set variables placeholders
 s_ph = tf.placeholder(tf.float32, name='ob1', shape=(None, obs_dim))
@@ -38,36 +40,43 @@ d_ph = tf.placeholder(tf.float32, name='dne', shape=(None,))
 
 # Create functions
 with tf.variable_scope('main'):
+    opt_action = create_nn(s_ph,hidden_sizes=hidden_sizes+[action_dim],scope='act', act_limit=max_act)
     opt_action_value = create_nn(
             tf.concat([a_ph,s_ph], axis=-1),
             scope='act-val', 
+            reuse=None,
             hidden_sizes=hidden_sizes+[1])
-    opt_action = create_nn(s_ph,hidden_sizes=hidden_sizes+[action_dim],scope='act')
-with tf.variable_scope('target'):
-    opt_action_value_target = create_nn(
-            tf.concat([a_ph,s_ph], axis=-1),
+    opt_action_value2 = create_nn(
+            tf.concat([opt_action,s_ph], axis=-1),
             scope='act-val', 
+            reuse=True,
             hidden_sizes=hidden_sizes+[1])
-    opt_action_target = create_nn(s_ph, hidden_sizes+[action_dim], scope='act')
+with tf.variable_scope('target'):
+    opt_action_target = create_nn(s_ph, hidden_sizes+[action_dim], scope='act', act_limit=max_act)
+    opt_action_value2_target = create_nn(
+            tf.concat([opt_action_target,s_ph], axis=-1),
+            scope='act-val',
+            reuse=None,
+            hidden_sizes=hidden_sizes+[1])
 
 # Create target value (y)
 # y = r + gamma*(1-d)*Qtar(s',atar'(s'))
-y = tf.stop_gradient(r_ph + gamma * (1-d_ph)*opt_action_value_target)
+y = tf.stop_gradient(r_ph + gamma * (1-d_ph)*opt_action_value2_target)
 
 # Create loss for optimal action-value function (Q* loss function)
 # loss = E(Q*(a,s)-y) = E((Q*(a,v)-(r+gamma*Q(s',a(s')))^2)
 loss_opt_act_val = tf.reduce_mean((opt_action_value - y)**2)
 
 # Create loss function for optimal deterministic policy (u(s))
-loss_opt_action = -tf.reduce_mean(opt_action_value)
+loss_opt_action = -tf.reduce_mean(opt_action_value2)
 
 # Creating optimizers
 opt_act_value_optimizer = tf.train.AdamOptimizer(learning_rate=lr_q) # TODO
 opt_action_optimizer    = tf.train.AdamOptimizer(learning_rate=lr_a) # TODO
 
 # Creating trainig operations
-opt_act_value_train = opt_act_value_optimizer.minimize(loss_opt_act_val)
-opt_action_train    = opt_action_optimizer.minimize(loss_opt_action)
+opt_act_value_train = opt_act_value_optimizer.minimize(loss_opt_act_val,var_list=get_vars('main/act-val'))
+opt_action_train    = opt_action_optimizer.minimize(loss_opt_action,var_list=get_vars('main/act'))
 
 # Actions to init target networks
 target_init = tf.group(
@@ -78,6 +87,10 @@ target_init = tf.group(
 target_update = tf.group(
         [tf.assign(target, p*target + (1-p)*original) for original, target in 
             zip(get_vars('main'),get_vars('target'))])
+
+# Count variables
+var_counts = tuple(count_vars(scope) for scope in ['main/act', 'main/act-val', 'main'])
+print('\nNumber of parameters: \t pi: %d, \t q: %d, \t total: %d\n'%var_counts)
 
 # Set buffer
 buf = ReplayBuffer(buffer_size, obs_dim, action_dim)
@@ -94,11 +107,20 @@ sess.run(target_init)
 # Main loop
 for epoch in range(num_epochs):
     # Episode
-    s = env.reset()
+    rewd, s = 0.0 , env.reset()
     for ep in range(ep_per_epoch):
-        a = get_action({s_ph: s.reshape(1,-1)}, opt_action, sess, max_act_val)
-        s2, r, done, _ = env.step(a[0])
-        env.render()
+
+        if random_steps > 0:
+            a = action_space.sample()
+            random_steps -= 1
+            if random_steps == 0:
+                print("Finished pure random episodes")
+        else:
+            a = get_action({s_ph: s.reshape(1,-1)}, opt_action, sess, max_act, action_dim)[0]
+
+        s2, r, done, _ = env.step(a)
+        rewd += r
+        #env.render()
 
         # Ignoring done signal if comes from end of episode
         # because d is about if the agent died because of a
@@ -113,7 +135,7 @@ for epoch in range(num_epochs):
         if d:
             break
 
-    print("Updating ...")
+    print("Updating (rewd %0.2f)..." % rewd)
         
     # Starting to update the parameters
     # Sample experiences 
--- utils.py
+++ utils.py
@@ -4,27 +4,35 @@ from pdb import set_trace
 
 def create_nn(input_ph, 
             hidden_sizes = (400,300), 
-            activations = tf.nn.relu, 
+            activations = tf.tanh,
             final_activation = None,
-            scope=None):
+            scope=None,
+            act_limit=1,
+            reuse=None,):
         ''' 
         Creates a NN based on parameters
         '''
-        with tf.variable_scope(scope):
+        print(hidden_sizes)
+        with tf.variable_scope(scope, reuse=reuse):
             for size in hidden_sizes[:-1]:
                     input_ph = tf.layers.dense(
                                     inputs=input_ph, 
                                     units = size, 
                                     activation = activations)
-        return tf.layers.dense(input_ph, units = hidden_sizes[-1], activation = final_activation)
+        return act_limit * tf.layers.dense(input_ph, units = hidden_sizes[-1], activation = final_activation)
 
-def get_action(feed_dict, opt_action, sess, max_value):
+def get_action(feed_dict, opt_action, sess, max_value, act_dim):
         '''
         Returns an action vector from the output of opt_action
         TODO: add Noise
         '''
         action = sess.run([opt_action], feed_dict=feed_dict)[0]
+        action += 0.1 * np.random.randn(act_dim)
         return np.clip(action, -max_value, max_value)
 
 def get_vars(scope):
     return [x for x in tf.global_variables() if scope in x.name]
+
+def count_vars(scope):
+    v = get_vars(scope)
+    return sum([np.prod(var.shape.as_list()) for var in v])
