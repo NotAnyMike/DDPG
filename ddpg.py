72ffc6ba0075fdc4c7b35f9718300ebbe06e3ee0
NotAnyMike <mikewoodcockc@gmail.com>
2019-01-20 19:53:30 +0000

    organizing code

--- ddpg.py
+++ ddpg.py
@@ -1,15 +1,17 @@
 # Imports
 import gym
 import tensorflow as tf
+import numpy as np
 from utils import create_nn, get_action, get_vars, count_vars
 from replay_buffer import ReplayBuffer
 from pdb import set_trace
 
 # Set Hyper-parameters
-num_epochs   = 1000
+seed         = 0
+num_epochs   = 100
 ep_per_epoch = 5000 #1000
 random_steps = 10000
-star_steps   = 100
+max_ep_len   = 1000
 hidden_sizes = [400,300]
 buffer_size  = int(1e6)
 batch_size   = 100
@@ -17,6 +19,10 @@ gamma        = 0.99
 lr_a         = 1e-3
 lr_q         = 1e-3
 p            = 0.995
+
+# Setting seed
+np.random.seed(seed)
+tf.set_random_seed(seed)
 
 # Create environment
 env = gym.make('BipedalWalker-v2')
@@ -28,37 +34,44 @@ obs_space    = env.observation_space
 action_dim   = action_space.shape[0]
 obs_dim      = obs_space.shape[0]
 max_act      = action_space.high[0] # TODO get high values for each dim
-print("obs", obs_dim)
-print("act", action_dim)
 
 # Set variables placeholders
-s_ph = tf.placeholder(tf.float32, name='ob1', shape=(None, obs_dim))
-a_ph = tf.placeholder(tf.float32, name='act', shape=(None, action_dim))
-r_ph = tf.placeholder(tf.float32, name='rwd', shape=(None,))
-s2_ph= tf.placeholder(tf.float32, name='ob2', shape=(None, obs_dim))
-d_ph = tf.placeholder(tf.float32, name='dne', shape=(None,))
+s_ph = tf.placeholder(tf.float32, shape=(None, obs_dim))
+a_ph = tf.placeholder(tf.float32, shape=(None, action_dim))
+r_ph = tf.placeholder(tf.float32, shape=(None,))
+s2_ph= tf.placeholder(tf.float32, shape=(None, obs_dim))
+d_ph = tf.placeholder(tf.float32, shape=(None,))
 
 # Create functions
 with tf.variable_scope('main'):
-    opt_action = create_nn(s_ph,hidden_sizes=hidden_sizes+[action_dim],scope='act', act_limit=max_act)
-    opt_action_value = create_nn(
-            tf.concat([a_ph,s_ph], axis=-1),
-            scope='act-val', 
-            reuse=None,
-            hidden_sizes=hidden_sizes+[1])
-    opt_action_value2 = create_nn(
-            tf.concat([opt_action,s_ph], axis=-1),
-            scope='act-val', 
-            reuse=True,
-            hidden_sizes=hidden_sizes+[1])
-with tf.variable_scope('target'):
-    opt_action_target = create_nn(s_ph, hidden_sizes+[action_dim], scope='act', act_limit=max_act)
-    opt_action_value2_target = create_nn(
-            tf.concat([opt_action_target,s_ph], axis=-1),
-            scope='act-val',
-            reuse=None,
-            hidden_sizes=hidden_sizes+[1])
 
+    with tf.variable_scope('pi'):
+        opt_action = create_nn(s_ph,hidden_sizes=hidden_sizes+[action_dim], act_limit=max_act)
+
+    with tf.variable_scope('q'):
+        opt_action_value = tf.squeeze(create_nn(
+                tf.concat([a_ph,s_ph], axis=-1),
+                hidden_sizes=hidden_sizes+[1]), axis=1)
+
+    with tf.variable_scope('q', reuse=True):
+        opt_action_value2 = tf.squeeze(create_nn(
+                tf.concat([opt_action,s_ph], axis=-1),
+                hidden_sizes=hidden_sizes+[1]), axis=1)
+
+with tf.variable_scope('target'):
+
+    with tf.variable_scope('pi'):
+        opt_action_target = create_nn(s_ph, hidden_sizes+[action_dim], act_limit=max_act)
+
+    with tf.variable_scope('q'):
+        opt_action_value2_target = tf.squeeze(create_nn(
+                tf.concat([opt_action_target,s_ph], axis=-1),
+                hidden_sizes=hidden_sizes+[1]), axis=1)
+
+# Count variables
+var_counts = tuple(count_vars(scope) for scope in ['main/pi', 'main/q', 'main'])
+
+print('\nNumber of parameters: \t pi: %d, \t q: %d, \t total: %d\n'%var_counts)
 # Create target value (y)
 # y = r + gamma*(1-d)*Qtar(s',atar'(s'))
 y = tf.stop_gradient(r_ph + gamma * (1-d_ph)*opt_action_value2_target)
@@ -75,8 +88,8 @@ opt_act_value_optimizer = tf.train.AdamOptimizer(learning_rate=lr_q) # TODO
 opt_action_optimizer    = tf.train.AdamOptimizer(learning_rate=lr_a) # TODO
 
 # Creating trainig operations
-opt_act_value_train = opt_act_value_optimizer.minimize(loss_opt_act_val,var_list=get_vars('main/act-val'))
-opt_action_train    = opt_action_optimizer.minimize(loss_opt_action,var_list=get_vars('main/act'))
+opt_action_train    = opt_action_optimizer.minimize(loss_opt_action,var_list=get_vars('main/pi'))
+opt_act_value_train = opt_act_value_optimizer.minimize(loss_opt_act_val,var_list=get_vars('main/q'))
 
 # Actions to init target networks
 target_init = tf.group(
@@ -88,73 +101,71 @@ target_update = tf.group(
         [tf.assign(target, p*target + (1-p)*original) for original, target in 
             zip(get_vars('main'),get_vars('target'))])
 
-# Count variables
-var_counts = tuple(count_vars(scope) for scope in ['main/act', 'main/act-val', 'main'])
-print('\nNumber of parameters: \t pi: %d, \t q: %d, \t total: %d\n'%var_counts)
-
 # Set buffer
 buf = ReplayBuffer(buffer_size, obs_dim, action_dim)
 
 # Start session and set dynamic memory
-config = tf.ConfigProto()
-config.gpu_options.allow_growth = True
-sess = tf.Session(config=config)
+#config = tf.ConfigProto()
+#config.gpu_options.allow_growth = True
+#sess = tf.Session(config=config)
+sess = tf.Session()
 
 # Initialize placeholders
-tf.global_variables_initializer().run(session=sess)
+sess.run(tf.global_variables_initializer())
 sess.run(target_init)
 
 # Main loop
-for epoch in range(num_epochs):
+ep_len, rewd, s = 0, 0.0 , env.reset()
+for t in range(num_epochs*ep_per_epoch):
     # Episode
-    rewd, s = 0.0 , env.reset()
-    for ep in range(ep_per_epoch):
 
-        if random_steps > 0:
-            a = action_space.sample()
-            random_steps -= 1
-            if random_steps == 0:
-                print("Finished pure random episodes")
-        else:
-            a = get_action({s_ph: s.reshape(1,-1)}, opt_action, sess, max_act, action_dim)[0]
+    if random_steps >= t:
+        a = action_space.sample()
+        if random_steps == t:
+            print("Finished pure random episodes")
+    else:
+        a = get_action({s_ph: s.reshape(1,-1)}, opt_action, sess, max_act, action_dim)[0]
 
-        s2, r, done, _ = env.step(a)
-        rewd += r
-        #env.render()
+    s2, r, done, _ = env.step(a)
+    rewd += r
+    #env.render()
 
-        # Ignoring done signal if comes from end of episode
-        # because d is about if the agent died because of a
-        # very bad action, not because the episode ended
-        # TODO could send wrong information
-        d = False if ep==ep_per_epoch else done
+    # Ignoring done signal if comes from end of episode
+    # because d is about if the agent died because of a
+    # very bad action, not because the episode ended
+    # TODO could send wrong information
+    d = False if ep_len == max_ep_len else done
 
-        # Store transition
-        buf.store(s,a,r,s2,d)
+    # Store transition
+    buf.store(s,a,r,s2,d)
+    s = s2
+    ep_len +=1
 
-        s = s2
-        if d:
-            break
+    # Update
+    if d or ep_len == max_ep_len:
+        print("Updating (t %i/%i, rewd %0.2f)..." % (t,num_epochs*ep_per_epoch,rewd))
 
-    print("Updating (rewd %0.2f)..." % rewd)
-        
-    # Starting to update the parameters
-    # Sample experiences 
-    D = buf.sample_branch(batch_size)
+        # Starting to update the parameters
+        # Sample experiences 
+        D = buf.sample_branch(batch_size)
 
-    # Creating feed_dic
-    feed_dict = {
-            s_ph: D['s'],
-            a_ph: D['a'],
-            r_ph: D['r'],
-            s2_ph:D['s2'],
-            d_ph: D['d']
-            }
+        # Creating feed_dic
+        feed_dict = {
+                s_ph: D['s'],
+                a_ph: D['a'],
+                r_ph: D['r'],
+                s2_ph:D['s2'],
+                d_ph: D['d']
+                }
 
-    # Update optimal action-value function (Q*)
-    sess.run([ loss_opt_act_val, opt_act_value_train], feed_dict=feed_dict)
+        # Update optimal action-value function (Q*)
+        sess.run([ loss_opt_act_val, opt_act_value_train], feed_dict=feed_dict)
 
-    # Update deterministic optimal action function (pi*)
-    sess.run([ loss_opt_action, opt_action_train], feed_dict=feed_dict)
+        # Update deterministic optimal action function (pi*)
+        sess.run([ loss_opt_action, opt_action_train], feed_dict=feed_dict)
 
-    # Update target functions TODO
-    sess.run([ target_update ], feed_dict=feed_dict)
+        # Update target functions
+        sess.run([ target_update ], feed_dict=feed_dict)
+
+        # Reset variables
+        ep_len, rewd, s = 0, 0.0 , env.reset()
--- utils.py
+++ utils.py
@@ -3,22 +3,18 @@ import numpy as np
 from pdb import set_trace
 
 def create_nn(input_ph, 
-            hidden_sizes = (400,300), 
+            hidden_sizes = [400,300], 
             activations = tf.tanh,
             final_activation = None,
-            scope=None,
-            act_limit=1,
-            reuse=None,):
+            act_limit=1):
         ''' 
         Creates a NN based on parameters
         '''
-        print(hidden_sizes)
-        with tf.variable_scope(scope, reuse=reuse):
-            for size in hidden_sizes[:-1]:
-                    input_ph = tf.layers.dense(
-                                    inputs=input_ph, 
-                                    units = size, 
-                                    activation = activations)
+        for size in hidden_sizes[:-1]:
+                input_ph = tf.layers.dense(
+                                inputs=input_ph, 
+                                units = size, 
+                                activation = activations)
         return act_limit * tf.layers.dense(input_ph, units = hidden_sizes[-1], activation = final_activation)
 
 def get_action(feed_dict, opt_action, sess, max_value, act_dim):
