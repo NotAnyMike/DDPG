8e2323b1659b31de602a7fd26c353ba7a0146e74
NotAnyMike <mikewoodcockc@gmail.com>
2019-01-31 09:17:49 +0000

    testing behaviors

--- carracing.py
+++ carracing.py
@@ -1,18 +1,19 @@
-import gym
+import gym, math
 from pdb import set_trace
 
 env = gym.make('CarRacing-v0')
 env.reset()
 env.render()
 for i in range(10000):
-    if i % 200 == 0 :
-        p = env.get_rnd_point_in_track()
-        env.reset()
-        print("reset")
+    #if i % 200 == 0 :
+    #    p = env.get_rnd_point_in_track()
+    #    env.reset()
     if i % 100 == 0:
-        print("placing")
-        p = env.get_rnd_point_in_track()
-        env.place_agent(p)
-    set_trace()
+        #p = env.get_rnd_point_in_track()
+        #env.place_agent(p)
+        #ang = env.env.car.hull.angle + math.pi/2
+        #env.env.car.hull.linearVelocity.Set(math.cos(ang)*100, math.sin(ang)*100)
+        #set_trace()
+        env.set_speed(100)
     env.render()
     env.step(env.action_space.sample()) # take a random action
