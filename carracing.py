e88d5acbf473b9e2570dd0d1e4c5fb3953e2bded
NotAnyMike <mikewoodcockc@gmail.com>
2019-01-31 06:56:21 +0000

    adding place_agent func

--- carracing.py
+++ carracing.py
@@ -1,9 +1,17 @@
 import gym
-#env = gym.make('CarRacing-v0')
+from pdb import set_trace
+
 env = gym.make('CarRacing-v0')
 env.reset()
 env.render()
 for i in range(10000):
-    if i % 100 == 0 : env.reset()
+    if i % 200 == 0 :
+        p = env.get_rnd_point_in_track()
+        env.reset()
+        print("reset")
+    if i % 100 == 0:
+        print("placing")
+        p = env.get_rnd_point_in_track()
+        env.place_agent(p)
     env.render()
     env.step(env.action_space.sample()) # take a random action
